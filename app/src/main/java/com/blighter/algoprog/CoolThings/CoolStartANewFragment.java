ce8c8760bac75abc81fd5f211448b0a7d66afbdf
Me <evgenji228@gmail.com>
2019-01-10 22:05:09 +0300

    New class for better ModuleFragment replacement

--- app/src/main/java/com/blighter/algoprog/CoolThings/CoolStartANewFragment.java
+++ app/src/main/java/com/blighter/algoprog/CoolThings/CoolStartANewFragment.java
@@ -1,4 +1,29 @@
 package com.blighter.algoprog.CoolThings;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.blighter.algoprog.Fragments.ModuleFragment;
+import com.blighter.algoprog.R;
+
 public class CoolStartANewFragment {
+    FragmentManager fragmentManager;
+    ModuleFragment fragment;
+    String id;
+    public CoolStartANewFragment(FragmentManager coolFragmentManager, ModuleFragment coolFragment, String coolId){
+        id = coolId;
+        fragment = coolFragment;
+        fragmentManager = coolFragmentManager;
+    }
+     public void startFragment(){
+         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+         Bundle bundle = new Bundle();
+         bundle.putString("id", id);
+         fragment.setArguments(bundle);
+         fragmentTransaction.replace(R.id.container_in_Main, fragment);
+         fragmentTransaction.addToBackStack(null);
+         fragmentTransaction.commit();
+     }
 }
