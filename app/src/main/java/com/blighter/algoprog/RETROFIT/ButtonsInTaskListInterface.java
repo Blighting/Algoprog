14dd693d9856f75971ee952ad53958ac1735d439
Me <evgenji228@gmail.com>
2019-02-21 21:14:58 +0300

    New class for lists with tasks

--- app/src/main/java/com/blighter/algoprog/RETROFIT/ButtonsInTaskListInterface.java
+++ app/src/main/java/com/blighter/algoprog/RETROFIT/ButtonsInTaskListInterface.java
@@ -1,4 +1,11 @@
 package com.blighter.algoprog.RETROFIT;
 
+import com.blighter.algoprog.POJO.MaterialsInTaskList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ButtonsInTaskListInterface {
+    @GET("material/{id}")
+    Call<MaterialsInTaskList> getTasks(String id);
 }
