af7712f89ab991c1a1d61b72980df2ed6e4db739
Me <evgenji228@gmail.com>
2019-03-10 00:49:01 +0300

    new POJO

--- app/src/main/java/com/blighter/algoprog/POJO/Materials.java
+++ app/src/main/java/com/blighter/algoprog/POJO/Materials.java
@@ -1,4 +1,22 @@
 package com.blighter.algoprog.POJO;
-
 public class Materials {
+    private String _id;
+
+    private String title;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
