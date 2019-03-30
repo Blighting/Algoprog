d91456c30706463d417e42a37bf1015d4d210bbf
Me <evgenji228@gmail.com>
2019-03-30 20:13:41 +0300

    Added TaskFragment

--- app/src/main/java/com/blighter/algoprog/fragments/TaskFragment.java
+++ app/src/main/java/com/blighter/algoprog/fragments/TaskFragment.java
@@ -1,4 +1,30 @@
-package com.blighter.algoprog.Fragments;
+package com.blighter.algoprog.fragments;
 
-public class TaskFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.blighter.algoprog.R;
+
+import static com.blighter.algoprog.api.ApiMethods.setTask;
+
+public class TaskFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        String id = null;
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            id = bundle.getString("idForTask");
+        }
+        WebView browser = (WebView) view.findViewById(R.id.wb_for_task);
+        setTask(id, browser, getContext());
+        return view;
+    }
 }
