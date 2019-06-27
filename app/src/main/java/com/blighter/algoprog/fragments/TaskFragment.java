f7ef46f9bc583ef44ca8b06d66f7b54d94b9930a
Me <evgenji228@gmail.com>
2019-06-27 16:41:08 +0300

    Added "задача на infromatics"

--- app/src/main/java/com/blighter/algoprog/fragments/TaskFragment.java
+++ app/src/main/java/com/blighter/algoprog/fragments/TaskFragment.java
@@ -1,13 +1,21 @@
 package com.blighter.algoprog.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.blighter.algoprog.R;
 
@@ -25,6 +33,27 @@ public class TaskFragment extends Fragment {
         }
         WebView browser = (WebView) view.findViewById(R.id.wb_for_task);
         setTask(id, browser, getContext());
+        TextView tv_informatics = (TextView) view.findViewById(R.id.tv_in_task_informatics);
+        final String link = "https://informatics.mccme.ru/moodle/mod/statements/view3.php?chapterid=" + id.replace("p", "");
+        tv_informatics.setMovementMethod(LinkMovementMethod.getInstance());
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                String url = link;
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                ds.setColor(getResources().getColor(R.color.colorForTextWithLink));
+                ds.setUnderlineText(false);
+            }
+        };
+        SpannableString coolSpannableStringForInformatics = new SpannableString(getString(R.string.infromatics));
+        coolSpannableStringForInformatics.setSpan(clickableSpan, 0, getString(R.string.infromatics).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv_informatics.setText(coolSpannableStringForInformatics);
         return view;
     }
 }
