242a085da43f1523e2801997dd15431954c90566
Me <evgenji228@gmail.com>
2019-07-07 18:46:50 +0300

    Interface for network request

--- app/src/main/java/com/blighter/algoprog/retrofit/BestSolutionsInterface.java
+++ app/src/main/java/com/blighter/algoprog/retrofit/BestSolutionsInterface.java
@@ -1,4 +1,15 @@
 package com.blighter.algoprog.retrofit;
 
+import com.blighter.algoprog.pojo.BestSolution;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
 public interface BestSolutionsInterface {
+    @GET("{task}")
+    Call<List<BestSolution>> getBestSolutions(@Header("Cookie") String cookies, @Path("task") String taskId);
 }
