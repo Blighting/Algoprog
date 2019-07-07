cc64217b64b1068fb5151fb4e77c247afcc64ad2
Me <evgenji228@gmail.com>
2019-07-07 18:46:40 +0300

    POJO

--- app/src/main/java/com/blighter/algoprog/pojo/BestSolution.java
+++ app/src/main/java/com/blighter/algoprog/pojo/BestSolution.java
@@ -1,4 +1,49 @@
 package com.blighter.algoprog.pojo;
 
 public class BestSolution {
+    private String downloadTime;
+    private Integer quality;
+    private String _id;
+    private String source;
+    private String language;
+    private String outcome;
+    private String problem;
+    private String user;
+    private String time;
+
+    public Integer getQuality() {
+        return quality;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDownloadTime() {
+        return downloadTime;
+    }
 }
