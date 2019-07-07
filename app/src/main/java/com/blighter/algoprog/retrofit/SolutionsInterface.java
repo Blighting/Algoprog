3241d48f0f0de02d93a83295b3f9a9a064bd4aed
Me <evgenji228@gmail.com>
2019-07-07 18:41:19 +0300

    Interface for network request

--- app/src/main/java/com/blighter/algoprog/retrofit/SolutionsInterface.java
+++ app/src/main/java/com/blighter/algoprog/retrofit/SolutionsInterface.java
@@ -1,4 +1,13 @@
 package com.blighter.algoprog.retrofit;
 
+import com.blighter.algoprog.pojo.Solutions;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
 public interface SolutionsInterface {
+    @GET("{task}")
+    Call<Solutions> getSolutions(@Header("Cookie") String cookies, @Path("task") String taskId);
 }
