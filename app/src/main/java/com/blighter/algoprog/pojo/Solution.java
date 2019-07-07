d6de90ab62629a53b0c336bdc75f562d1d9a5c6f
Me <evgenji228@gmail.com>
2019-07-07 18:40:58 +0300

    POJO

--- app/src/main/java/com/blighter/algoprog/pojo/Solution.java
+++ app/src/main/java/com/blighter/algoprog/pojo/Solution.java
@@ -1,4 +1,55 @@
 package com.blighter.algoprog.pojo;
 
+
 public class Solution {
+    private String downloadTime;
+    private String[] comments;
+    private boolean force;
+    private int quality;
+    private String _id;
+    private Results results;
+    private String source;
+    private String time;
+    private String outcome;
+    private String language;
+
+    public String getDownloadTime() {
+        return downloadTime;
+    }
+
+    public String[] getComments() {
+        return comments;
+    }
+
+    public boolean isForce() {
+        return force;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public Results getResults() {
+        return results;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
 }
