1362b95ca818e7c846f64f9743b4f8ead5c60b10
Me <evgenji228@gmail.com>
2019-07-23 15:33:59 +0300

    RxAndroid 2 Update

--- app/src/main/java/com/blighter/algoprog/retrofit/BestSolutionsInterface.java
+++ app/src/main/java/com/blighter/algoprog/retrofit/BestSolutionsInterface.java
@@ -4,12 +4,12 @@ import com.blighter.algoprog.pojo.BestSolution;
 
 import java.util.List;
 
-import retrofit2.Call;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
 
 public interface BestSolutionsInterface {
-    @GET("{task}")
-    Call<List<BestSolution>> getBestSolutions(@Header("Cookie") String cookies, @Path("task") String taskId);
+    @GET("bestSubmits/{task}")
+    Observable<List<BestSolution>> getBestSolutions(@Header("Cookie") String cookies, @Path("task") String taskId);
 }
