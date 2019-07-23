669a6aad26a8fc51c7eb01c7c0d5b68fce2251df
Me <evgenji228@gmail.com>
2019-07-23 15:30:45 +0300

    RxAndroid 2 Update

--- app/src/main/java/com/blighter/algoprog/retrofit/MyUserInterface.java
+++ app/src/main/java/com/blighter/algoprog/retrofit/MyUserInterface.java
@@ -2,11 +2,11 @@ package com.blighter.algoprog.retrofit;
 
 import com.blighter.algoprog.pojo.myUser;
 
-import retrofit2.Call;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 
 public interface MyUserInterface {
     @GET("myUser")
-    Call<myUser> getMyUserInfo(@Header("Cookie") String cookies);
+    Observable<myUser> getMyUserInfo(@Header("Cookie") String cookies);
 }
