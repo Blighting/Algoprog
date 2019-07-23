68f895aa2eda86dce44b1653bf67522fa4c6cc53
Me <evgenji228@gmail.com>
2019-07-23 15:34:23 +0300

    RxAndroid 2 Update

--- app/src/main/java/com/blighter/algoprog/retrofit/AuthorizationInterface.java
+++ app/src/main/java/com/blighter/algoprog/retrofit/AuthorizationInterface.java
@@ -3,11 +3,12 @@ package com.blighter.algoprog.retrofit;
 import com.blighter.algoprog.pojo.Cookies;
 import com.blighter.algoprog.pojo.UserData;
 
-import retrofit2.Call;
+import io.reactivex.Observable;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface AuthorizationInterface {
     @POST("login")
-    Call<Cookies> getCookies(@Body UserData userData);
+    Observable<Response<Cookies>> getCookies(@Body UserData userData);
 }
