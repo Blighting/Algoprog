a85d6ca749e9f835e5d3cd3443e797721231621b
Me <evgenji228@gmail.com>
2019-07-23 15:29:13 +0300

    initial commit

--- app/src/main/java/com/blighter/algoprog/pojo/Test.java
+++ app/src/main/java/com/blighter/algoprog/pojo/Test.java
@@ -1,4 +1,10 @@
 package com.blighter.algoprog.pojo;
 
 public class Test {
+    private Integer time;
+    private String status;
+    private String string_status;
+    private Integer real_time;
+    private String max_memory_used;
+
 }
