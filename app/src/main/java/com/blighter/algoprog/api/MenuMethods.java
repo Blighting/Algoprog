74b649935a151f9ed63b574983a76334bffa9221
Me <evgenji228@gmail.com>
2019-07-24 18:29:40 +0300

    Changed implementations of SharedPreferences names

--- app/src/main/java/com/blighter/algoprog/api/MenuMethods.java
+++ app/src/main/java/com/blighter/algoprog/api/MenuMethods.java
@@ -3,13 +3,13 @@ package com.blighter.algoprog.api;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import static com.blighter.algoprog.fragments.LoginFragment.APP_PREFERENCES;
+import com.blighter.algoprog.R;
 
 
 public class MenuMethods {
     //выход из системы
     public static void menuExit(Context context, android.support.v7.app.ActionBar actionBar) {
-        SharedPreferences sharedPref = context.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.app_preferences), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.clear();
         editor.apply();
