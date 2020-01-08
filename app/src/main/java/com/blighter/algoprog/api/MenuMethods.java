41b01935b12dbfd43bd72e5f44f9be6c826c346d
Evgeny <34033078+Blighting@users.noreply.github.com>
2020-01-08 12:38:50 +0300

    New Login logic

--- app/src/main/java/com/blighter/algoprog/api/MenuMethods.java
+++ app/src/main/java/com/blighter/algoprog/api/MenuMethods.java
@@ -5,13 +5,18 @@ import android.content.SharedPreferences;
 
 import com.blighter.algoprog.R;
 
-
+//this class contains methods which are interact with menu and can be used in several fragments
 public class MenuMethods {
-    //выход из системы
+    //this method sets user information as unknown(he got no cookies) and ActionBar
     public static void menuExit(Context context, androidx.appcompat.app.ActionBar actionBar) {
         SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.app_preferences), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.clear();
+        editor.remove(context.getString(R.string.password));
+        editor.remove(context.getString(R.string.user_id));
+        editor.remove(context.getString(R.string.login));
+        editor.remove(context.getString(R.string.cookies));
+        editor.putBoolean(context.getString(R.string.noCookies), true);
+        editor.putBoolean(context.getString(R.string.oldCookies), true);
         editor.apply();
         actionBar.setTitle("Неизвестный Пользователь");
         actionBar.setSubtitle("");
